3d13dca2ad6ddc06c51055c8851640f9a087b2e9
zlatimirCS <zlatimir_rk@yahoo.com>
2023-12-19 15:21:01 +0100

    parsing incoming requests

--- 01_NODEJS/01_basic/app.js
+++ 01_NODEJS/01_basic/app.js
@@ -1,15 +1,20 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/add-product', (req, res, next) => {
-  console.log('first middleware');
-  res.send('<h1>Hello add product!</h1>');
+  res.send('<form action="product" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-  console.log('third middleware');
   res.send('<h1>Hello from Express!</h1>');
 });
 
--- 01_NODEJS/README.js
+++ 01_NODEJS/README.js
@@ -307,3 +307,27 @@ app.use('/', (req, res, next) => {
 
 app.listen(3000);
 —-----------------------------------------------------
+  // npm install body - parser--save
+  ------------------------------------------------------
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="product" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+  console.log('body', req.body);
+  res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
+------------------------------------------------------
