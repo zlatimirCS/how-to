59755be3dc6403f98dfe62cc0d89845fa4a1c6bf
zlatimirCS <zlatimir_rk@yahoo.com>
2024-01-05 09:14:02 +0100

    finished with pug templates

--- 01_NODEJS/02_dynamic_content/routes/admin.js
+++ 01_NODEJS/02_dynamic_content/routes/admin.js
@@ -6,7 +6,10 @@ const rootDir = require("../util/path");
 const products = [];
 
 router.get("/add-product", (req, res, next) => {
-  res.render("add-product", { pageTitle: "Add Product" });
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+  });
 });
 
 router.post("/product", (req, res, next) => {
