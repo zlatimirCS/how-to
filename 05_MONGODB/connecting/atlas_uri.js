89ce8268b978e50a9f3c07f0feda99f81622b90d
zlatimirCS <zlatimir_rk@yahoo.com>
2024-04-01 11:26:39 +0200

    removed mongo uri

--- 05_MONGODB/connecting/atlas_uri.js
+++ 05_MONGODB/connecting/atlas_uri.js
@@ -1,2 +1,2 @@
-module.exports = uri =
-  "mongodb+srv://zlatimir_ramac:0LzDqTDxooJxNSuY@cluster0.jmtwni8.mongodb.net/test";
+// module.exports = uri =
+//   "mongodb+srv://zlatimir_ramac:0LzDqTDxooJxNSuY@cluster0.jmtwni8.mongodb.net/test";
